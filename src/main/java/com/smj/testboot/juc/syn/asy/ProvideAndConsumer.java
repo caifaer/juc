7b51cba970bd1cb23ed02d65352266905d7471da
shen'meng'jie <565611652@qq.com>
2021-11-01 21:47:00 +0800

    生产者 消费者模式

---
+++ src/main/java/com/smj/testboot/juc/syn/asy/ProvideAndConsumer.java
@@ -0,0 +1,114 @@
+package com.smj.testboot.juc.syn.asy;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedList;
+
+@Slf4j(topic = "c.ProvideAndConsumer")
+public class ProvideAndConsumer {
+
+    public static void main(String[] args) {
+        MessageQueue queue = new MessageQueue(2);
+
+        for (int i = 0; i < 3; i++) {
+            int id = i;
+            new Thread(() -> {
+                queue.put(new Message(id , "值"+id));
+            }, "生产者" + i).start();
+        }
+
+        new Thread(() -> {
+            while(true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message message = queue.take();
+            }
+        }, "消费者").start();
+    }
+
+}
+
+// 消息队列类 ， java 线程之间通信
+@Slf4j(topic = "c.MessageQueue")
+class MessageQueue {
+    // 消息的队列集合
+    private LinkedList<Message> list = new LinkedList<>();
+    // 队列容量
+    private int capcity;
+
+    public MessageQueue(int capcity) {
+        this.capcity = capcity;
+    }
+
+    // 获取消息
+    public Message take() {
+        // 检查队列是否为空
+        synchronized (list) {
+            while(list.isEmpty()) {
+                try {
+                    log.debug("队列为空, 消费者线程等待");
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 从队列头部获取消息并返回
+            Message message = list.removeFirst();
+            log.debug("已消费消息 {}", message);
+            // 因为可能队列已满 生产者会阻塞 通知生产者继续生产
+            list.notifyAll();
+            return message;
+        }
+    }
+
+    // 存入消息
+    public void put(Message message) {
+        synchronized (list) {
+            // 检查对象是否已满
+            while(list.size() == capcity) {
+                try {
+                    log.debug("队列已满, 生产者线程等待");
+                    list.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 将消息加入队列尾部
+            list.addLast(message);
+            log.debug("已生产消息 {}", message);
+            // 通知消费者 去消费
+            list.notifyAll();
+        }
+    }
+}
+
+// 加了final  子类不可重写  更加保险
+final class Message {
+    private int id;
+    private Object value;
+
+    public Message(int id, Object value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    // 只有get方法  属性不可变 即只读  线程安全
+    public int getId() {
+        return id;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", value=" + value +
+                '}';
+    }
+}
