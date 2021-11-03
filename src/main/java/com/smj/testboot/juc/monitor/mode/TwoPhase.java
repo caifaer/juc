6ad3e1c46b891cae65d63265cf5d8f38ad05d637
shen'meng'jie <565611652@qq.com>
2021-11-03 21:25:24 +0800

    两阶段终止模式说明

--- src/main/java/com/smj/testboot/juc/monitor/mode/TwoPhase.java
+++ src/main/java/com/smj/testboot/juc/monitor/mode/TwoPhase.java
@@ -1,10 +1,21 @@
-package com.smj.testboot.juc;
+package com.smj.testboot.juc.monitor.mode;
 
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j(topic = "c.TwoPhase")
 public class TwoPhase {
+
+    /**
+     * 两阶段终止模式 在一个线程 T1 中如何“优雅”终止线程 T2？这里的【优雅】指的是给 T2 一个料理后事的机会。
+     *
+     *   总的来说  两阶段 终止模式 就是使用 interrupt
+     *              所以有两种情况  一种就是 打断  sleep/wait/join(会清除打断标记)
+     *                              一种就是 打断 普通线程(不会清除)
+     *
+     */
+
+
 
     public static void main(String[] args) {
 
