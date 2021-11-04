59f9e9fb1178ac46e348f25481bd0549a9589756
shen'meng'jie <565611652@qq.com>
2021-11-04 21:47:29 +0800

    dcl

---
+++ src/main/java/com/smj/testboot/juc/memory/mode/DoubleCheckLocking.java
@@ -0,0 +1,27 @@
+package com.smj.testboot.juc.memory.mode;
+
+public class DoubleCheckLocking {
+
+
+}
+
+
+final class Singleton {
+    private Singleton() { }
+
+    // 使用volatile 来避免 有序性问题
+    private static volatile Singleton INSTANCE = null;
+
+    public static Singleton getInstance() {
+        // 实例没创建，才会进入内部的 synchronized代码块
+        if (INSTANCE == null) {
+            synchronized (Singleton.class) { // t2
+                // 也许有其它线程已经创建实例，所以再判断一次
+                if (INSTANCE == null) { // t1
+                    INSTANCE = new Singleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
