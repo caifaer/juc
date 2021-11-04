9bc247877ec23d406a29a5fccd487f9f1f310b6a
shen'meng'jie <565611652@qq.com>
2021-11-04 21:45:21 +0800

    有序性--指令重排序

---
+++ src/main/java/com/smj/testboot/juc/memory/profile/TestOrder.java
@@ -0,0 +1,14 @@
+package com.smj.testboot.juc.memory.profile;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.TestOrder")
+public class TestOrder {
+    /**
+     *   有序性
+     *   指令重排序   可以使用 volatile 禁止指令重排序
+     */
+
+
+}
