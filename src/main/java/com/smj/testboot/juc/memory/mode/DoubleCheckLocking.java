6daafbae8f2e169eaf36a30c487475f3c9102d6e
shen'meng'jie <565611652@qq.com>
2021-11-06 16:29:18 +0800

    dcl

--- src/main/java/com/smj/testboot/juc/memory/mode/DoubleCheckLocking.java
+++ src/main/java/com/smj/testboot/juc/memory/mode/DoubleCheckLocking.java
@@ -10,6 +10,8 @@ final class Singleton {
     private Singleton() { }
 
     // 使用volatile 来避免 有序性问题
+    // synchronized 可以保证有序性  但是其中还是会发生指令重排
+    // 本质问题还是 创建对象的时候并不是原子性操作  还是会发生指令重排
     private static volatile Singleton INSTANCE = null;
 
     public static Singleton getInstance() {
